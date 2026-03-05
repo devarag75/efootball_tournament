4bea579c5fb9ea37ef2db173e086101ba1876f1f
devarag75 <devaragpd831@gmail.com>
2026-03-05 21:26:05 +0530

    zz

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/efootball_tournament/',
     plugins: [
         react(),
         tailwindcss(),
